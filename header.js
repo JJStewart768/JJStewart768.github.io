f0e684b53e4b63b54f40009c9cbdb1ab07d8f9f6
JJ Stewart <jstewa12@tufts.edu>
2023-12-07 01:00:26 -0500

    Images and Header
    Added more images, and added in a header.js file

---
+++ header.js
@@ -0,0 +1,5 @@
+document.write("<header><div class='nav'>" +
+    "<a href='https://www.tiktok.com/@tacobell'><img class='planetlink' src='./images/TIKTOK.png' /></a>" +
+    "<a href='https://www.instagram.com/tacobell/'><img class='planetlink' src='./images/INSTAGRAM.png' /></a>" +
+    "<a href='https://twitter.com/tacobell'><img class='planetlink' src='./images/TWITTER.png' /></a></div></header>"
+);
