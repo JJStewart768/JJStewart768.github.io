a1ed6820ae008cf9880863e04b6e6535764d52ce
JJ Stewart <jstewa12@tufts.edu>
2023-12-08 09:02:48 -0500

    Final Edits!
    Final edits to sizing of assets, especially food items

--- dollarbill.js
+++ dollarbill.js
@@ -1,32 +1,32 @@
 document.write(
     "<div id='fadeIn' class='container'>" +
-        "<img class='fooditems' id='breakfast' src='./images/BREAKFAST.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='burrito' src='./images/BURRITO.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='cinnabon1' src='./images/CINNABON1.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='cinnabon2' src='./images/CINNABON2.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='cinnabon3' src='./images/CINNABON1.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='cinnabon4' src='./images/CINNABON2.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='potatotaco' src='./images/POTATOTACO.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='stacker' src='./images/STACKER.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='fooditems' id='tostada' src='./images/TOSTADA.png'>" +
-            "<a href=''></a>" +
-        "</img>" +
-        "<img class='dollarbill' src='./images/DOLLARBILL.png' /></img>" +
+        "<a href='https://www.tacobell.com/food/breakfast/cheesy-toasted-breakfast-burrito-bacon'>" +
+            "<img class='fooditems' id='breakfast' src='./images/BREAKFAST.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/burritos/cheesy-bean-and-rice-burrito'>" +
+            "<img class='fooditems' id='burrito' src='./images/BURRITO.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/sides-sweets/cinnabon-delights-12-pack'>" +
+            "<img class='fooditems' id='cinnabon1' src='./images/CINNABON1.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/sides-sweets/cinnabon-delights-12-pack'>" +
+            "<img class='fooditems' id='cinnabon2' src='./images/CINNABON2.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/sides-sweets/cinnabon-delights-12-pack'>" +
+            "<img class='fooditems' id='cinnabon3' src='./images/CINNABON1.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/sides-sweets/cinnabon-delights-12-pack'>" +
+            "<img class='fooditems' id='cinnabon4' src='./images/CINNABON2.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/tacos/spicy-potato-soft-taco'>" +
+            "<img class='fooditems' id='potatotaco' src='./images/POTATOTACO.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/cravings-value-menu'>" +
+            "<img class='fooditems' id='stacker' src='./images/STACKER.png' />" +
+        "</a>" +
+        "<a href='https://www.tacobell.com/food/cravings-value-menu'>" +
+            "<img class='fooditems' id='tostada' src='./images/TOSTADA.png' />" +
+        "</a>" +
+        "<img class='dollarbill' src='./images/DOLLARBILL.png'>" +
     "</div>"
 );
