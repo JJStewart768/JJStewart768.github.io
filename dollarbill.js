15aa728a26b8faaeff8436d27f04f79cd633bd3c
JJ Stewart <jstewa12@tufts.edu>
2023-12-08 02:22:04 -0500

    FUN
    We're having FUN

--- dollarbill.js
+++ dollarbill.js
@@ -1,14 +1,32 @@
 document.write(
     "<div id='fadeIn' class='container'>" +
-        "<img class='fooditems' href='' id='breakfast' src='./images/BREAKFAST.png' />" +
-        "<img class='fooditems' href='' id='burrito' src='./images/BURRITO.png' />" +
-        "<img class='fooditems' href='' id='cinnabon1' src='./images/CINNABON1.png' />" +
-        "<img class='fooditems' href='' id='cinnabon2' src='./images/CINNABON2.png' />" +
-        "<img class='fooditems' href='' id='cinnabon3' src='./images/CINNABON1.png' />" +
-        "<img class='fooditems' href='' id='cinnabon4' src='./images/CINNABON2.png' />" +
-        "<img class='fooditems' href='' id='potatotaco' src='./images/POTATOTACO.png' />" +
-        "<img class='fooditems' href='' id='stacker' src='./images/STACKER.png' />" +
-        "<img class='fooditems' href='' id='tostada' src='./images/TOSTADA.png' />" +
-        "<img class='dollarbill' src='./images/DOLLARBILL.png' />" +
+        "<img class='fooditems' id='breakfast' src='./images/BREAKFAST.png'>" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='burrito' src='./images/BURRITO.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='cinnabon1' src='./images/CINNABON1.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='cinnabon2' src='./images/CINNABON2.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='cinnabon3' src='./images/CINNABON1.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='cinnabon4' src='./images/CINNABON2.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='potatotaco' src='./images/POTATOTACO.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='stacker' src='./images/STACKER.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='fooditems' id='tostada' src='./images/TOSTADA.png' />" +
+            "<a href=''></a>"
+        "</img>" +
+        "<img class='dollarbill' src='./images/DOLLARBILL.png' /></img>" +
     "</div>"
 );
