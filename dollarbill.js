4dfe9de0ebf4cb62f1a335d649a5b26de606d3af
JJ Stewart <jstewa12@tufts.edu>
2023-12-08 02:26:59 -0500

    Fixed issue
    Made sure the image worked!

--- dollarbill.js
+++ dollarbill.js
@@ -1,31 +1,31 @@
 document.write(
     "<div id='fadeIn' class='container'>" +
         "<img class='fooditems' id='breakfast' src='./images/BREAKFAST.png'>" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='burrito' src='./images/BURRITO.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='cinnabon1' src='./images/CINNABON1.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='cinnabon2' src='./images/CINNABON2.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='cinnabon3' src='./images/CINNABON1.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='cinnabon4' src='./images/CINNABON2.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='potatotaco' src='./images/POTATOTACO.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='stacker' src='./images/STACKER.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='fooditems' id='tostada' src='./images/TOSTADA.png' />" +
-            "<a href=''></a>"
+            "<a href=''></a>" +
         "</img>" +
         "<img class='dollarbill' src='./images/DOLLARBILL.png' /></img>" +
     "</div>"
